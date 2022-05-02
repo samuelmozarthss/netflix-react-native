94f5a0443dd37fd86731fceb21ee6027f39a7b46
samuelmozarthss <contatosamuelm@gmail.com>
2022-05-02 00:00:32 -0300

    Firebase Cloud Messaging configuration

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert} from 'react-native';
 
 import Home from './screen/Home';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
   return <Home />;
 };
 export default App;
--- index.js
+++ index.js
@@ -3,7 +3,21 @@
  */
 
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
-AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(remoteMessage => {
+  console.log('Nofitication arrived', remoteMessage);
+});
+
+function HeadlessCheck({isHeadless}) {
+  if (isHeadless) {
+    return null;
+  }
+
+  return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
